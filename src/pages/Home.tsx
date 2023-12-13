a6d3eb5c178f845ee380d48e39a914fd0b153899
julio <juliosequeiros@ipvc.pt>
2023-12-13 22:23:59 +0000

    home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,10 @@
 import {
-    IonButtons, IonCard,
-    IonContent,
-    IonHeader,
+    IonButton,
+    IonButtons, IonCard, IonCol,
+    IonContent, IonGrid,
+    IonHeader, IonIcon, IonImg,
     IonMenuButton,
-    IonPage,
+    IonPage, IonRow, IonText,
     IonTitle,
     IonToolbar
 } from "@ionic/react";
@@ -41,13 +42,73 @@ return (
                 <IonTitle>Início</IonTitle>
             </IonToolbar>
         </IonHeader>
-
         <IonContent fullscreen>
             <IonHeader collapse="condense">
                 <IonToolbar>
                     <IonTitle size="large">Início</IonTitle>
                 </IonToolbar>
             </IonHeader>
+        </IonContent>
+        <IonContent>
+            <IonGrid>
+                <IonRow>
+                    <IonCol>
+                        <IonImg src="https://example.com/images/store.png" />
+                    </IonCol>
+                    <IonCol>
+                        <IonButton>
+                            <IonIcon slot="icon-only" name="map" />
+                            Navegar para a loja mais próxima
+                        </IonButton>
+                    </IonCol>
+                </IonRow>
+                <IonRow>
+                    <IonCol>
+                        <IonText>
+                            <h2>Apresentação da empresa</h2>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+                                tincidunt, neque sit amet fermentum scelerisque, eros urna
+                                tristique neque, ac dapibus eros risus sed sapien. Aliquam
+                                fermentum, arcu vel luctus scelerisque, leo enim semper
+                            </p>
+                                <IonRow>
+                                <IonCol>
+                                    <IonImg src="https://example.com/images/black-friday.png" />
+                                </IonCol>
+                                <IonCol>
+                                    <IonButton>
+                                        <IonIcon slot="icon-only" name="card" />
+                                        Saiba mais sobre a Black Friday
+                                    </IonButton>
+                                </IonCol>
+                            </IonRow>
+                                <IonRow>
+                                    <IonCol>
+                                        <IonImg src="https://example.com/images/awards.png" />
+                                    </IonCol>
+                                    <IonCol>
+                                        <IonButton>
+                                            <IonIcon slot="icon-only" name="star" />
+                                            Veja nossos prêmios
+                                        </IonButton>
+                                    </IonCol>
+                                </IonRow>
+                                <IonRow>
+                                    <IonCol>
+                                        <IonImg src="https://example.com/images/cars.png" />
+                                    </IonCol>
+                                    <IonCol>
+                                        <IonButton>
+                                            <IonIcon slot="icon-only" name="car" />
+                                            Conheça nossos novos carros
+                                        </IonButton>
+                                    </IonCol>
+                                </IonRow>
+                        </IonText>
+                    </IonCol>
+                </IonRow>
+            </IonGrid>
         </IonContent>
     </IonPage>
 );
