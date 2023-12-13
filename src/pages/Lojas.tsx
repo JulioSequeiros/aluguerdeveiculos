bd8646a6291e06169d888fdd453e8ec3049fc9fc
JoaoRodri04 <joaopaulorodrigues@ipvc.pt>
2023-12-13 19:13:45 +0000

    a

--- src/pages/Lojas.tsx
+++ src/pages/Lojas.tsx
@@ -1,136 +1,104 @@
-import React from 'react';
+import React, {ReactNode, useState} from 'react';
 import {
     IonButtons,
     IonContent,
     IonHeader,
-    IonMenuButton,
-    IonPage,
+    IonList,
+    IonItem,
+    IonModal,
     IonTitle,
-    IonToolbar,
+    IonImg,
     IonCard,
-    IonCardContent,
     IonCardHeader,
     IonCardSubtitle,
-    IonCardTitle,
-    IonItem,
-    IonLabel,
-    IonList,
-    IonThumbnail, IonButton,
-} from "@ionic/react";
-import './Lojas.css';
-import { Geolocation } from '@capacitor/geolocation';
+    IonCardContent, IonToolbar, IonSearchbar, IonButton, IonPage,
+} from '@ionic/react';
 
-const Lojas: React.FC = () => {
-    const printCurrentPosition = async () => {
-        const coordinates = await Geolocation.getCurrentPosition();
+interface Store {
+    name: string;
+    address: string;
+    imagem: string;
+}
 
-        console.log('Current position:', coordinates);
+const generateRandomPrice = () => {
+    const randomPrice = (Math.random() * (150 - 50) + 50).toFixed(2);
+    return `€${randomPrice}`;
+};
+
+function IonSubtitle(props: { children: ReactNode }) {
+    return null;
+}
+
+const Frota: React.FC = () => {
+    const [showModal, setShowModal] = useState(false);
+    const [selectedStore, setSelectedStore] = useState<Store | null>(null);
+
+    const handleOpenModal = (store: Store) => {
+        setSelectedStore(store);
+        setShowModal(true);
     };
+
+    const handleCloseModal = () => {
+        setShowModal(false);
+        setSelectedStore(null);
+    };
+
     const stores = [
         {
-            name: 'Viana do Castelo',
-            address: 'Rua da Ribeira, 5000-558 Viana do Castelo',
-            phoneNumber: '+351 258 802 605',
+            name: "Loja 1",
+            address: "Rua da Loja, 123, Viana do Castelo",
+            imagem: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/Viana_do_Castelo_Panorama.jpg/1200px-Viana_do_Castelo_Panorama.jpg",
         },
         {
-            name: 'Braga',
-            address: 'Rua do Pão de Ló, 4700-062 Braga',
-            phoneNumber: '+351 253 275 028',
+            name: "Loja 2",
+            address: "Rua da Loja, 456, Porto",
+            imagem: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/70/Porto_Panorama.jpg/1200px-Porto_Panorama.jpg",
         },
         {
-            name: 'Porto',
-            address: 'Rua Santa Catarina, 4000-042 Porto',
-            phoneNumber: '+351 222 095 090',
+            name: "Loja 3",
+            address: "Rua da Loja, 789, Braga",
+            imagem: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/06/Braga_Panorama.jpg/1200px-Braga_Panorama.jpg",
         },
         {
-            name: 'Lisboa',
-            address: 'Rua do Ouro, 1170-764 Lisboa',
-            phoneNumber: '+351 213 100 010',
+            name: "Loja 4",
+            address: "Rua da Loja, 1011, Lisboa",
+            imagem: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/Lisboa_Panorama.jpg/1200px-Lisboa_Panorama.jpg",
         },
     ];
-    const showModal = (store) => {
-        const modal = document.createElement('ion-modal');
-        modal.id = 'store-modal';
-        const header = document.createElement('ion-header');
-        header.textContent = `Informações da Loja: ${store.name}`;
-        const content = document.createElement('ion-content');
-        const title = document.createElement('ion-title');
-        title.textContent = 'Nome: ${store.name}';
-        const address = document.createElement('ion-subtitle');
-        address.textContent = 'Endereço: ${store.address}';
-        const phoneNumber = document.createElement('ion-subtitle');
-        phoneNumber.textContent = 'Número de telefone: ${store.phoneNumber}';
-        content.appendChild(title);
-        content.appendChild(address);
-        content.appendChild(phoneNumber);
-        modal.appendChild(header);
-        modal.appendChild(content);
-        document.body.appendChild(modal);
 
-        modal.addEventListener('ion-close', () => {
-            modal.remove();
-        });
-    };
-        return (
-            <IonPage>
-                <IonHeader translucent={true}>
-                    <IonToolbar>
-                        <IonButtons slot="start">
-                            <IonMenuButton/>
-                        </IonButtons>
-                        <IonTitle>Lojas</IonTitle>
-                    </IonToolbar>
-                </IonHeader>
+    return (
+        <IonPage>
+            <IonHeader>
+                <IonToolbar>
+                    <IonButtons slot="start">
+                        <IonSearchbar />
+                    </IonButtons>
+                    <IonTitle>Lojas</IonTitle>
+                </IonToolbar>
+            </IonHeader>
 
-                <IonContent fullscreen>
-                    <IonHeader collapse="condense">
-                        <IonToolbar>
-                            <IonTitle size="large">Lojas</IonTitle>
-                        </IonToolbar>
-                    </IonHeader>
-                    <IonCard>
-                        <IonCardHeader>
-                            <IonCardTitle>Localizações</IonCardTitle>
-                        </IonCardHeader>
-                        <IonCardContent>
-                            <IonList>
-                                <IonItem>
-                                    <IonItem>
-                                        <IonThumbnail slot="start">
-                                            <img alt="Store logo" src="https://example.com/store-logo.png" />
-                                        </IonThumbnail>
-                                        <IonLabel>{stores.name}</IonLabel>
-                                        <IonButton onClick={() => showModal(stores)}>Ver Informações</IonButton>
-                                    </IonItem>
-                                </IonItem>
+            <IonContent fullscreen>
+                <IonList>
+                    {stores.map((store, index) => (
+                        <IonItem key={index}>
+                            <IonTitle>{store.name}</IonTitle>
+                            <IonSubtitle>{store.address}</IonSubtitle>
+                            <IonButton onClick={() => handleOpenModal(store)}>Mais informações</IonButton>
+                        </IonItem>
+                    ))}
+                </IonList>
 
-                                <IonItem>
-                                    <IonThumbnail slot="start">
-                                        <img alt="Silhouette of mountains" src="https://cdn.discordapp.com/attachments/1021814484437835837/1184194027667607622/loja.jpg?ex=658b15a1&is=6578a0a1&hm=b5edc80d1fc2e116eb36cf1206f078e58a882a11c9d5cebbcda58fa66d593a67&" />
-                                    </IonThumbnail>
-                                    <IonLabel>Braga</IonLabel>
-                                </IonItem>
+                {selectedStore && (
+                    <IonModal isOpen={showModal} onDidDismiss={handleCloseModal}>
+                        <IonContent>
+                            <IonTitle><h2>{selectedStore.name}</h2></IonTitle>
+                            <IonImg src={selectedStore.imagem}></IonImg>
+                        </IonContent>
+                    </IonModal>
+                )}
+            </IonContent>
+        </IonPage>
+    );
+};
 
-                                <IonItem>
-                                    <IonThumbnail slot="start">
-                                        <img alt="Silhouette of mountains" src="https://cdn.discordapp.com/attachments/1021814484437835837/1184194027667607622/loja.jpg?ex=658b15a1&is=6578a0a1&hm=b5edc80d1fc2e116eb36cf1206f078e58a882a11c9d5cebbcda58fa66d593a67&" />
-                                    </IonThumbnail>
-                                    <IonLabel>Porto</IonLabel>
-                                </IonItem>
-
-                                <IonItem lines="none">
-                                    <IonThumbnail slot="start">
-                                        <img alt="Silhouette of mountains" src="https://cdn.discordapp.com/attachments/1021814484437835837/1184194027667607622/loja.jpg?ex=658b15a1&is=6578a0a1&hm=b5edc80d1fc2e116eb36cf1206f078e58a882a11c9d5cebbcda58fa66d593a67&" />
-                                    </IonThumbnail>
-                                    <IonLabel>Lisboa</IonLabel>
-                                </IonItem>
-                            </IonList>
-                        </IonCardContent>
-                    </IonCard>
-                </IonContent>
-            </IonPage>
-        );
-    };
-
-
-export default Lojas;
+export default Frota
